c869a8aa481aea027d050d87f6200b58dc675afa
yflau17 <jdmatt126@gmail.com>
2023-01-14 21:39:06 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+#weather-app
+
+![Screenshot](./Screenshot.png)
---
+++ script.js
@@ -0,0 +1,39 @@
+import { config } from './config.js';
+
+let weather = {
+    apiKey: config.key,
+    fetchWeather: function(city) {
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + this.apiKey
+        ).then((response) => response.json()).then((data) => this.displayWeather(data));
+    },
+    displayWeather: function(data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        document.querySelector(".city").innerText = "Weather in " + name; 
+        document.querySelector(".icon").src = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
+        document.querySelector(".temp").innerText = temp + "°C";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+        document.querySelector(".wind").innerHTML = "Wind speed: " + speed + "km/h";
+        document.querySelector(".weather").classList.remove("loading");
+        document.body.style.backgroundImage = "url('https://source.unsplash.com/random/1600x900/?" + name +"')"
+    },
+    search: function() {
+        this.fetchWeather(document.querySelector(".search-bar").value);
+    },
+};
+
+document.querySelector(".search button").addEventListener("click", function() {
+    weather.search();
+})
+
+document.querySelector(".search-bar").addEventListener("keyup", function(event) {
+    if (event.key == "Enter") {
+        weather.search();
+    }
+})
+
+weather.fetchWeather("London");
